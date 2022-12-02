9e70e83b06fcea63c3f3be594bf8e84fa7f90d75
BVC SME 01 <118875477+bvc-sme01@users.noreply.github.com>
2022-12-01 18:21:10 -0700

    Make context change

--- App.tsx
+++ App.tsx
@@ -3,10 +3,17 @@ import MainScreen from "./screens/MainScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import TemperatureUnit from "./types/TemperatureUnit";
 import TemperatureUnitContext from "./context/TemperatureUnitContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const [temperatureUnit, setTemperatureUnit] = useState<TemperatureUnit>("F");
+  const [temperatureUnit, setTemperatureUnit] = useState<TemperatureUnit>("C");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTemperatureUnit("F");
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <SafeAreaProvider>
