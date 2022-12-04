6d5fd9143f5c68e9f5078aabaa7bf1db1a2e06f6
BVC SME 01 <118875477+bvc-sme01@users.noreply.github.com>
2022-12-04 15:08:42 -0700

    POST location from form

--- api/locations.ts
+++ api/locations.ts
@@ -6,3 +6,14 @@ export function getLocations() {
     response.json()
   );
 }
+
+export function postLocation(location: Omit<Location, "id">) {
+  return fetch(BASE_URL + "locations/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(location),
+  }).then<Location>((response) => response.json());
+}
