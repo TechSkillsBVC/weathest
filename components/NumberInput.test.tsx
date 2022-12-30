a3298fb1ba6c1ac5b7bee7d30fa6a4b42d72f928
BVC SME 01 <118875477+bvc-sme01@users.noreply.github.com>
2022-12-29 20:16:34 -0700

    Add more tests for NumberInput

--- components/NumberInput.test.tsx
+++ components/NumberInput.test.tsx
@@ -1,4 +1,9 @@
-import { render, screen } from "@testing-library/react-native";
+import {
+  RenderResult,
+  fireEvent,
+  render,
+  screen,
+} from "@testing-library/react-native";
 
 import NumberInput from "./NumberInput";
 
@@ -12,5 +17,136 @@ describe(NumberInput.name, () => {
     const { rerender } = render(<NumberInput value={50} />);
     rerender(<NumberInput value={100} />);
     expect(screen.getByTestId("input")).toHaveProp("value", "100");
+  });
+
+  describe("when user inputs", () => {
+    let changeValueHandler: jest.Mock;
+    let input: ReturnType<RenderResult["getByTestId"]>;
+
+    beforeEach(() => {
+      changeValueHandler = jest.fn();
+      render(<NumberInput onChangeValue={changeValueHandler} />);
+      input = screen.getByTestId("input");
+    });
+
+    describe("integer", () => {
+      beforeEach(() => fireEvent.changeText(input, "50"));
+
+      it("calls `onChangeValue` prop", () => {
+        expect(changeValueHandler).toBeCalledWith(50);
+      });
+
+      it("displays value", () => {
+        expect(input).toHaveProp("value", "50");
+      });
+    });
+
+    describe("zero", () => {
+      beforeEach(() => fireEvent.changeText(input, "0"));
+
+      it("calls `onChangeValue` prop", () => {
+        expect(changeValueHandler).toBeCalledWith(0);
+      });
+
+      it("displays value", () => {
+        expect(input).toHaveProp("value", "0");
+      });
+    });
+
+    describe("decimal", () => {
+      beforeEach(() => fireEvent.changeText(input, "50.5"));
+
+      it("calls `onChangeValue` prop", () => {
+        expect(changeValueHandler).toBeCalledWith(50.5);
+      });
+
+      it("displays value", () => {
+        expect(input).toHaveProp("value", "50.5");
+      });
+    });
+
+    describe("negative decimal", () => {
+      beforeEach(() => fireEvent.changeText(input, "-50.5"));
+
+      it("calls `onChangeValue` prop", () => {
+        expect(changeValueHandler).toBeCalledWith(-50.5);
+      });
+
+      it("displays value", () => {
+        expect(input).toHaveProp("value", "-50.5");
+      });
+    });
+
+    describe("invalid number --482", () => {
+      beforeEach(() => fireEvent.changeText(input, "--482"));
+
+      it("calls `onChangeValue` prop with sanitized value", () => {
+        expect(changeValueHandler).toBeCalledWith(-482);
+      });
+
+      it("displays sanitized value", () => {
+        expect(input).toHaveProp("value", "-482");
+      });
+    });
+
+    describe("invalid number 45-8", () => {
+      beforeEach(() => fireEvent.changeText(input, "45-8"));
+
+      it("calls `onChangeValue` prop with sanitized value", () => {
+        expect(changeValueHandler).toBeCalledWith(458);
+      });
+
+      it("displays sanitized value", () => {
+        expect(input).toHaveProp("value", "458");
+      });
+    });
+
+    describe("invalid number 42.4.3.2", () => {
+      beforeEach(() => fireEvent.changeText(input, "42.4.3.2"));
+
+      it("calls `onChangeValue` prop with sanitized value", () => {
+        expect(changeValueHandler).toBeCalledWith(42.432);
+      });
+
+      it("displays sanitized value", () => {
+        expect(input).toHaveProp("value", "42.432");
+      });
+    });
+
+    describe("invalid number 42foo.2", () => {
+      beforeEach(() => fireEvent.changeText(input, "42foo.2"));
+
+      it("calls `onChangeValue` prop with sanitized value", () => {
+        expect(changeValueHandler).toBeCalledWith(42.2);
+      });
+
+      it("displays sanitized value", () => {
+        expect(input).toHaveProp("value", "42.2");
+      });
+    });
+
+    describe("number with trailing dot", () => {
+      beforeEach(() => fireEvent.changeText(input, "123."));
+
+      it("calls `onChangeValue` prop with sanitized value", () => {
+        expect(changeValueHandler).toBeCalledWith(123);
+      });
+
+      it("displays value with trailing dot, allowing user to continue inputting a decimal", () => {
+        expect(input).toHaveProp("value", "123.");
+      });
+    });
+
+    describe("non-number text", () => {
+      beforeEach(() => fireEvent.changeText(input, "foo"));
+
+      it("calls `onChangeValue` prop with sanitized null`", () => {
+        expect(changeValueHandler).toBeCalledWith(null);
+      });
+
+      it("displays empty field", () => {
+        expect(input).toHaveProp("value", "");
+      });
+    });
   });
 });
